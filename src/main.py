4444cc1ea09720e96850549d98b372727120cd52
PamodiRanmal <ranmalmendis8@gmail.com>
2022-11-30 09:36:16 +0530

    checking the pdf format based on contents

--- src/main.py
+++ src/main.py
@@ -4,11 +4,6 @@ from datetime import datetime
 import pandas as pd
 from dateutil import parser
 
-name = ""
-policy_num = ""
-effective_date = ""
-expiry_date = ""
-
 print(fitz.__doc__)
 results = {}
 
@@ -24,6 +19,9 @@ def pdf_extractor(pdf_path):
 
     for num in range(num_of_pages):
         page = pdf[num]
+
+        # checking the pdf format
+        check_format = format_checker(page)
 
         # get all annotations on a page
         annots = page.annots()
@@ -54,33 +52,39 @@ def pdf_extractor(pdf_path):
         dates = []
 
         for i in highlighted_contents:
-            if name_regex.match(i):
-                global name
-                name = i
-                results['Name of Insured'] = name
+            if check_format == 1:
+                if name_regex.match(i):
+                    name = i
+                    results['Name of Insured'] = name
 
-            elif number_regex.match(i):
-                global policy_num
-                policy_num = i
-                results['Policy Number'] = policy_num
+                elif number_regex.match(i):
+                    policy_num = i
+                    results['Policy Number'] = policy_num
 
-            elif date_regex1.match(i):
-                dates.append(i[:-1])
+                elif date_regex1.match(i):
+                    dates.append(i[:-1])
 
-            elif date_regex2.match(i):
-                date_time = parser.parse(i)
-                # print('date time', date_time)
-                strdate = str(pd.to_datetime(i))
-                formated = strdate.split(' ')[0]
-                dates.append(formated.replace('-', '/'))
+            elif check_format == 2:
+                if name_regex.match(i):
+                    name = i
+                    results['Name of Insured'] = name
 
-        # print('dates are ', dates)
+                elif number_regex.match(i):
+                    policy_num = i
+                    results['Policy Number'] = policy_num
+
+                elif date_regex2.match(i):
+                    date_time = parser.parse(i)
+                    strdate = str(pd.to_datetime(i))
+                    formated = strdate.split(' ')[0]
+                    dates.append(formated.replace('-', '/'))
+
         date_checker(dates)
 
         print(f'''
         ----------------------------------------------------------------------------------------
         Sample PDF Output:
-    
+
         Name of Insured: {results['Name of Insured']}
         Policy Number: {results['Policy Number']}
         Effective Date: {results['Effective Date']}
@@ -91,21 +95,32 @@ def pdf_extractor(pdf_path):
         return results
 
 
+def format_checker(page):
+    search_string1 = page.search_for("Economical Mutual Insurance Company")
+    search_string2 = page.search_for("Definity Insurance Company")
+
+    if len(search_string1) != 0:
+        pdf_format = 1
+        return pdf_format
+
+    elif len(search_string2) != 0:
+        pdf_format = 2
+        return pdf_format
+
+
 def date_checker(dates):
     d1 = pd.to_datetime(dates[0])
     d2 = pd.to_datetime(dates[1])
 
     if d1 < d2:
-        global expiry_date
         expiry_date = dates[1]
         global results
         results['Expiry Date'] = expiry_date
-        global effective_date
         effective_date = dates[0]
         results['Effective Date'] = effective_date
 
 
-pdf_extractor('../Resources/pdf_sample_format1.pdf')
+pdf_extractor('../Resources/pdf_sample_format2.pdf')
 
 if __name__ == '__main__':
     pass
